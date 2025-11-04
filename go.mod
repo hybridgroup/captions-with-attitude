6adb26893af30f9ac541677a08bd36ecf280a2b6
deadprogram <ron@hybridgroup.com>
2025-11-04 11:40:38 +0100

    all: able to serve mjpeg on local server page
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hybridgroup/video-description
+
+go 1.25.0
+
+require (
+	github.com/hybridgroup/mjpeg v0.0.0-20250330094202-16d243df0e35
+	gocv.io/x/gocv v0.42.0
+)
---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	_ "embed"
+
+	"github.com/hybridgroup/mjpeg"
+)
+
+//go:embed html/index.html
+var index string
+
+func startServer(host string, stream *mjpeg.Stream) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(index))
+	})
+	mux.Handle("/video", stream)
+
+	server := &http.Server{
+		Addr:         host,
+		Handler:      mux,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
